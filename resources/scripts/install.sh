569735666d379c354182968a69c3eb70b66a8b44
Juan Martin Gonzalez <juanmartin.gonzalez@nimbux911.com>
2022-08-22 16:49:19 -0300

    update files

--- resources/scripts/install.sh
+++ resources/scripts/install.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-apt-get update
-apt-get install docker.io docker-compose awscli python3-certbot python3-certbot-dns-route53 -y
-usermod -aG docker ubuntu
+# Move ssh service to port 2222
 
-sudo certbot certonly --non-interactive --agree-tos --email ${email} --no-redirect --dns-route53 -d ${dns}
-sudo service docker restart
-sudo mkswap /dev/nvme1n1
-sudo swapon /dev/nvme1n1
-sudo echo "/dev/nvme1n1 none swap sw 0 0" >> /etc/fstab
 sudo echo "Port 2222" >> /etc/ssh/sshd_config
+sudo echo "PubkeyAcceptedKeyTypes +ssh-rsa" >> /etc/ssh/sshd_config
+
 sudo systemctl restart sshd
-
-export GITLAB_HOME=/home/ubuntu/gitlab
-chown -R ubuntu:ubuntu /home/ubuntu/gitlab
-
-cd /home/ubuntu/gitlab
-docker-compose up -d 
-sudo mkdir -p /home/ubuntu/gitlab/config/ssl
-sudo chmod 755 ssl
-sudo cp /etc/letsencrypt/live/${dns}/fullchain.pem /home/ubuntu/gitlab/config/ssl/${dns}.crt
-sudo cp /etc/letsencrypt/live/${dns}/privkey.pem /home/ubuntu/gitlab/config/ssl/${dns}.key
