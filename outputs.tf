963533494f81637ddc62c5ab64b284d03b73f034
Diego Forziati <diego.forziati@nimbux911.com>
2022-12-15 15:40:40 -0300

    erasing outputs

--- outputs.tf
+++ outputs.tf
@@ -5,11 +5,3 @@ output "gitlab_instance_id" {
 output "gitlab_volume_id" {
   value = var.gitlab_snapshot_id != null ? aws_ebs_volume.gitlab_snapshot[0].id : aws_ebs_volume.gitlab.id
 }
-
-output "keypair_name" {
-  value = aws_key_pair.this.key_name
-}
-
-output "ssm_private_key_name" {
-  value = aws_ssm_parameter.private_key.name
-}
