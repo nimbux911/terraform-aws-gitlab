eac3b6580eb3c89bb13225b04931a131804cdac3
Ian Mika Ursino <ianmikaursino@gmail.com>
2026-06-17 14:14:57 -0300

    fix ubuntu image value

--- data.tf
+++ data.tf
@@ -13,7 +13,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-<26.04-codename>-26.04-amd64-server-*"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-resolute-26.04-amd64-server-*"]
   }
 
   filter {
